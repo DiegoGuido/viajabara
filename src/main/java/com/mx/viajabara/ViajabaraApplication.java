39393b993667840738589f4e723def52272354df
Diego Guido <20203tn026@utez.edu.mx>
2023-10-08 19:47:07 -0600

    initial commit

---
+++ src/main/java/com/mx/viajabara/ViajabaraApplication.java
@@ -0,0 +1,13 @@
+package com.mx.viajabara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViajabaraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViajabaraApplication.class, args);
+	}
+
+}
